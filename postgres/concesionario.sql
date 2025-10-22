fb462f3e76e65234ef1b4134151a3b5dbeebd63a
ArmandoEstuco <ledro5466@gmail.com>
2025-10-21 19:01:07 -0500

    se suben los archivos

---
+++ postgres/concesionario.sql
@@ -0,0 +1,10 @@
+create table "propietarios" (
+"id_propietario" int not null, 
+"Nombre1" text not null, 
+"Nombre2" text null, 
+"Apellido1" text not null, 
+"Apellido2" text null, 
+"Fecha_nac" date null,
+primary key (id_propietario)
+);
+
